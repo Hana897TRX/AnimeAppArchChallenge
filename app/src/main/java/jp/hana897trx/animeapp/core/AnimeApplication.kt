02938712151c98719c652776beffe3bb4f3029b2
Yuurei <yuureivt@gmail.com>
2023-08-23 22:10:45 -0800

    First commit

---
+++ app/src/main/java/jp/hana897trx/animeapp/core/AnimeApplication.kt
@@ -0,0 +1,7 @@
+package jp.hana897trx.animeapp.core
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class AnimeApplication : Application()
---
+++ app/src/main/java/jp/hana897trx/animeapp/core/CoreDI.kt
@@ -0,0 +1,11 @@
+package jp.hana897trx.animeapp.core
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+class CoreDI {
+
+}
