d97818436a566fe14dd838765993bb9891f8f822
Yuurei <yuureivt@gmail.com>
2023-09-16 21:52:26 -0800

    Remote APP module

--- buildSrc/src/main/java/Modules.kt
+++ buildSrc/src/main/java/Modules.kt
@@ -1,5 +1,5 @@
 object Modules {
-    const val APP = ":app"
+    const val APP = ":appcore"
     const val COMMON = ":common"
     const val DATA = ":data"
     const val DOMAIN = ":domain"
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,7 +14,6 @@ dependencyResolutionManagement {
 }
 
 rootProject.name = "animeAppChallenge"
-include(":app")
 include(":data")
 include(":domain")
 include(":common")
